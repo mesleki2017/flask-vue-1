a205e9c1b660777893ea9291902002e657353fb3
mesleki2017 <mesleki2017@gmail.com>
2021-11-09 15:48:28 +0300

    ok

--- deneme.py
+++ deneme.py
@@ -1,21 +1,36 @@
-
-from flask_socketio import SocketIO
-from flask import Flask, request, render_template
+from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "random string"
+##CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///list1.db"
+# Optional: But it will silence the deprecation warning in the console.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-socketio = SocketIO(app)
+
+##CREATE TABLE
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(250),nullable=False)
+    done = db.Column(db.Boolean)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Todo {self.task}>'
 
 
+def create_db():
+    db.create_all()
 
-@app.route('/',methods=['GET', 'POST'])#decorator
-def sessions():
-    return render_template('index5.html')
+    # CREATE RECORD
+    new_book = Todo(task="fff", done=False)
+    db.session.add(new_book)
+    db.session.commit()
 
-if __name__ == '__main__':
-    socketio.run(app,host='127.0.0.1',debug=True,port=5000)
+task = Todo.query.filter_by(task="fff").first()
+print(task.id)
+
+
 

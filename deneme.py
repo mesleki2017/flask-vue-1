6ba6805f68051359019bbc087832db92e12f266c
mesleki2017 <mesleki2017@gmail.com>
2021-11-09 16:44:39 +0300

    karisik durum

--- deneme.py
+++ deneme.py
@@ -1,14 +1,22 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
 
 ##CREATE DATABASE
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///list1.db"
 # Optional: But it will silence the deprecation warning in the console.
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy(app)    
 
+
+@app.route('/',methods=['GET', 'POST'])#decorator
+def sessions():
+    return render_template('index5.html')
 
 ##CREATE TABLE
 class Todo(db.Model):
@@ -32,5 +40,20 @@ def create_db():
 task = Todo.query.filter_by(task="fff").first()
 print(task.id)
 
+@socketio.on('addDataToBackendSql')
+def tabloyaEkleBackend(FrontenddenGelenData, methods=['GET', 'POST']):
+    ipadr=request.remote_addr
+    new_task = Todo(task=FrontenddenGelenData["title"], done=FrontenddenGelenData["done"])
+    db.session.add(new_task)
+    db.session.commit()
+
+@socketio.on('connect_to_server')
+def tumSqlDatasiniGonder(FrontenddenGelenData, methods=['GET', 'POST']):
+    print(FrontenddenGelenData)
+    tasks22 = Todo.query.all()
+    print(type(tasks22))
+    socketio.emit('backend_sends_all_list',"tasks22")
 
 
+if __name__ == '__main__':
+    socketio.run(app,host='127.0.0.1',debug=True,port=5000)

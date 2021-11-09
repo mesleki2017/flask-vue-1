6abb255f2852e9f84d43aa0f6e67c8db728f0abb
mesleki2017 <mesleki2017@gmail.com>
2021-11-09 14:19:30 +0300

    ok

--- deneme.py
+++ deneme.py
@@ -5,41 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///aaa.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 app.config['SECRET_KEY'] = "random string"
-db = SQLAlchemy(app)
+
 socketio = SocketIO(app)
 
- # Define ORM
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(120), unique=True)
-    done = db.Column(db.Boolean)
-
-    def __init__(self, title, description, done):
-        self.title = title
-        self.description = description
-        self.done = done
-
-    def __repr__(self):
-        return '<Todo %r>' % self.title
-        
-def create_db():
-    # Recreate database each time for demo
-    #db.drop_all()
-    db.create_all()
-
-
-create_db()
-
-
-tasks = [Todo('Buy groceries', 'Milk, Cheese, Pizza, Fruit, Tylenol', False),
-             Todo('Learn Python', 'Need to find a good Python tutorial on the web', False),
-             Todo('Mow the lawn', 'Find out some tools', False)]
-db.session.add_all(tasks)
-db.session.commit()
 
 
 @app.route('/',methods=['GET', 'POST'])#decorator
